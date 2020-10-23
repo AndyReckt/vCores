cf7560527c684c9d8379bbf2fd60fe9aea3cb252
Yochran <71285258+Yochran@users.noreply.github.com>
2020-10-23 11:08:02 -0400

    Update vBasic.java

--- src/main/java/net/vectromc/vbasic/vBasic.java
+++ src/main/java/net/vectromc/vbasic/vBasic.java
@@ -1,11 +1,15 @@
 package net.vectromc.vbasic;
 
 import net.vectromc.vbasic.commands.*;
+import net.vectromc.vbasic.listeners.ChatListener;
 import net.vectromc.vbasic.listeners.PlayerLogEvents;
+import net.vectromc.vbasic.listeners.SettingsClickListeners;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.UUID;
 
 public final class vBasic extends JavaPlugin {
@@ -27,6 +31,10 @@ public final class vBasic extends JavaPlugin {
     }
 
     public ArrayList<UUID> toggle_staff_alerts = new ArrayList<>();
+    public ArrayList<UUID> tpm = new ArrayList<>();
+    public ArrayList<UUID> tms = new ArrayList<>();
+    public ArrayList<UUID> tgc = new ArrayList<>();
+    public HashMap<String, String> reply = new HashMap<>();
 
     private void startupAnnouncements() {
         System.out.println("[VectroMC] vBasic v1.0 by Yochran is loading...");
@@ -52,10 +60,18 @@ public final class vBasic extends JavaPlugin {
         getCommand("gma").setExecutor(new GamemodeCommands());
         getCommand("heal").setExecutor(new HealCommand());
         getCommand("feed").setExecutor(new FeedCommand());
+        getCommand("message").setExecutor(new MsgCommand());
+        getCommand("reply").setExecutor(new MsgCommand());
+        getCommand("toggleglobalchat").setExecutor(new ToggleGlobalChat());
+        getCommand("togglemessagesounds").setExecutor(new ToggleMessageSounds());
+        getCommand("togglemessages").setExecutor(new ToggleMessages());
+        getCommand("settings").setExecutor(new SettingsCommand());
     }
 
     private void registerEvents() {
         PluginManager manager = getServer().getPluginManager();
         manager.registerEvents(new PlayerLogEvents(), this);
+        manager.registerEvents(new ChatListener(), this);
+        manager.registerEvents(new SettingsClickListeners(), this);
     }
 }
