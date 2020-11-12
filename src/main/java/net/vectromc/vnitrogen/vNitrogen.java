a01a771399c858efa62a2b44cb67bd7c44dda07b
Yochran <71285258+Yochran@users.noreply.github.com>
2020-11-11 19:23:09 -0500

    Update vNitrogen.java

--- src/main/java/net/vectromc/vnitrogen/vNitrogen.java
+++ src/main/java/net/vectromc/vnitrogen/vNitrogen.java
@@ -21,6 +21,8 @@ import net.vectromc.vnitrogen.listeners.starterlisteners.ACStarterListener;
 import net.vectromc.vnitrogen.listeners.starterlisteners.BCStarterListener;
 import net.vectromc.vnitrogen.listeners.starterlisteners.MCStarterListener;
 import net.vectromc.vnitrogen.listeners.starterlisteners.SCStarterListener;
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -40,8 +42,9 @@ public final class vNitrogen extends JavaPlugin {
         registerEvents();
         registerCommands();
         runRunnables();
-        registerRanks();
         registerData();
+        registerRanks();
+        refreshBlacklists();
         refreshBans();
         refreshMutes();
         startupAnnouncements();
@@ -53,18 +56,17 @@ public final class vNitrogen extends JavaPlugin {
         // Plugin shutdown logic
         shutdownAnnouncements();
     }
-
-    // HashMaps
-    public HashMap<String, Integer> ranks = new HashMap<>();
-
-    // ArrayLists
+    
     public ArrayList<UUID> buildchat_toggle = new ArrayList<>();
     public ArrayList<UUID> staffchat_toggle = new ArrayList<>();
     public ArrayList<UUID> adminchat_toggle = new ArrayList<>();
     public ArrayList<UUID> managementchat_toggle = new ArrayList<>();
+    public ArrayList<String> ranks = new ArrayList<>();
     public ArrayList<String> muted = new ArrayList<>();
     public ArrayList<String> banned = new ArrayList<>();
-    public  ArrayList<String> alts = new ArrayList<>();
+    public ArrayList<String> blacklisted = new ArrayList<>();
+    public ArrayList<String> alts = new ArrayList<>();
+    public ArrayList<String> chatMute = new ArrayList<>();
 
     private void startupAnnouncements() {
         System.out.println("[VectroMC] vNitrogen v1.0 by Yochran is loading...");
@@ -101,6 +103,10 @@ public final class vNitrogen extends JavaPlugin {
         getCommand("TempMute").setExecutor(new TempmuteCommand());
         getCommand("TempBan").setExecutor(new TempbanCommand());
         getCommand("Alts").setExecutor(new AltsCommand());
+        getCommand("MuteChat").setExecutor(new MutechatCommand());
+        getCommand("ClearChat").setExecutor(new ClearChatCommand());
+        getCommand("Blacklist").setExecutor(new BlacklistCommand());
+        getCommand("Unblacklist").setExecutor(new UnblacklistCommand());
     }
 
     private void registerEvents() {
@@ -125,6 +131,7 @@ public final class vNitrogen extends JavaPlugin {
         manager.registerEvents(new MuteChatListener(), this);
         manager.registerEvents(new BanJoinListener(), this);
         manager.registerEvents(new GUIClickListener(), this);
+        manager.registerEvents(new BlacklistJoinListener(), this);
     }
 
     private void runRunnables() {
@@ -132,148 +139,67 @@ public final class vNitrogen extends JavaPlugin {
     }
 
     public void registerRanks() {
-        System.out.println("[VectroMC] Registering all ranks...");
-        this.ranks.put("Owner", 1);
-        this.ranks.put("Developer", 2);
-        this.ranks.put("Manager", 3);
-        this.ranks.put("Admin", 4);
-        this.ranks.put("Senior-Mod", 5);
-        this.ranks.put("Mod", 6);
-        this.ranks.put("Trial-Mod", 7);
-        this.ranks.put("Builder", 8);
-        this.ranks.put("Default", 9);
-        System.out.println("[VectroMC] All ranks registered successfully.");
+        ranks.clear();
+        for (String rank : getConfig().getConfigurationSection("Ranks").getKeys(false)) {
+            ranks.add(rank);
+        }
+        for (String test : this.ranks) {
+            String permName = getConfig().getString("Ranks." + test + ".permission");
+            System.out.println(test + ", " + permName);
+        }
     }
 
     public void setPlayerPrefix(Player player) {
-        if (player.hasPermission("vnitrogen.groups.owner")) {
-            player.setDisplayName(getConfig().getString("Owner.prefix") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.developer")) {
-            player.setDisplayName(getConfig().getString("Developer.prefix") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.manager")) {
-            player.setDisplayName(getConfig().getString("Manager.prefix") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.admin")) {
-            player.setDisplayName(getConfig().getString("Admin.prefix") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.seniormod")) {
-            player.setDisplayName(getConfig().getString("Senior-Mod.prefix") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.mod")) {
-            player.setDisplayName(getConfig().getString("Mod.prefix") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.trialmod")) {
-            player.setDisplayName(getConfig().getString("Trial-Mod.prefix") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.builder")) {
-            player.setDisplayName(getConfig().getString("Builder.prefix") + player.getName());
-        } else {
-            player.setDisplayName(getConfig().getString("Default.prefix") + player.getName());
+        for (String rank : this.ranks) {
+            if (player.hasPermission(getConfig().getString("Ranks." + rank + ".permission"))) {
+                String prefix = getConfig().getString("Ranks." + rank + ".prefix");
+                player.setDisplayName(ChatColor.translateAlternateColorCodes('&', prefix + player.getName()));
+            }
         }
     }
 
     public void setPlayerColor(Player player) {
-        if (player.hasPermission("vnitrogen.groups.owner")) {
-            player.setDisplayName(getConfig().getString("Owner.color") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.developer")) {
-            player.setDisplayName(getConfig().getString("Developer.color") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.manager")) {
-            player.setDisplayName(getConfig().getString("Manager.color") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.admin")) {
-            player.setDisplayName(getConfig().getString("Admin.color") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.seniormod")) {
-            player.setDisplayName(getConfig().getString("Senior-Mod.color") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.mod")) {
-            player.setDisplayName(getConfig().getString("Mod.color") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.trialmod")) {
-            player.setDisplayName(getConfig().getString("Trial-Mod.color") + player.getName());
-        } else if (player.hasPermission("vnitrogen.groups.builder")) {
-            player.setDisplayName(getConfig().getString("Builder.color") + player.getName());
-        } else {
-            player.setDisplayName(getConfig().getString("Default.color") + player.getName());
+        for (String rank : this.ranks) {
+            if (player.hasPermission(getConfig().getString("Ranks." + rank + ".permission"))) {
+                String color = getConfig().getString("Ranks." + rank + ".color");
+                player.setDisplayName(ChatColor.translateAlternateColorCodes('&', color + player.getName()));
+            }
         }
     }
 
     public void setTargetPrefix(Player target) {
-        if (target.hasPermission("vnitrogen.groups.owner")) {
-            target.setDisplayName(getConfig().getString("Owner.prefix") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.developer")) {
-            target.setDisplayName(getConfig().getString("Developer.prefix") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.manager")) {
-            target.setDisplayName(getConfig().getString("Manager.prefix") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.admin")) {
-            target.setDisplayName(getConfig().getString("Admin.prefix") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.seniormod")) {
-            target.setDisplayName(getConfig().getString("Senior-Mod.prefix") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.mod")) {
-            target.setDisplayName(getConfig().getString("Mod.prefix") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.trialmod")) {
-            target.setDisplayName(getConfig().getString("Trial-Mod.prefix") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.builder")) {
-            target.setDisplayName(getConfig().getString("Builder.prefix") + target.getName());
-        } else {
-            target.setDisplayName(getConfig().getString("Default.prefix") + target.getName());
+        for (String rank : this.ranks) {
+            if (target.hasPermission(getConfig().getString("Ranks." + rank + ".permission"))) {
+                String prefix = getConfig().getString("Ranks." + rank + ".prefix");
+                target.setDisplayName(ChatColor.translateAlternateColorCodes('&', prefix + target.getName()));
+            }
         }
     }
 
     public void setTargetColor(Player target) {
-        if (target.hasPermission("vnitrogen.groups.owner")) {
-            target.setDisplayName(getConfig().getString("Owner.color") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.developer")) {
-            target.setDisplayName(getConfig().getString("Developer.color") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.manager")) {
-            target.setDisplayName(getConfig().getString("Manager.color") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.admin")) {
-            target.setDisplayName(getConfig().getString("Admin.color") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.seniormod")) {
-            target.setDisplayName(getConfig().getString("Senior-Mod.color") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.mod")) {
-            target.setDisplayName(getConfig().getString("Mod.color") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.trialmod")) {
-            target.setDisplayName(getConfig().getString("Trial-Mod.color") + target.getName());
-        } else if (target.hasPermission("vnitrogen.groups.builder")) {
-            target.setDisplayName(getConfig().getString("Builder.color") + target.getName());
-        } else {
-            target.setDisplayName(getConfig().getString("Default.color") + target.getName());
+        for (String rank : this.ranks) {
+            if (target.hasPermission(getConfig().getString("Ranks." + rank + ".permission"))) {
+                String color = getConfig().getString("Ranks." + rank + ".color");
+                target.setDisplayName(ChatColor.translateAlternateColorCodes('&', color + target.getName()));
+            }
         }
     }
 
     public void setTarget2Prefix(Player target2) {
-        if (target2.hasPermission("vnitrogen.groups.owner")) {
-            target2.setDisplayName(getConfig().getString("Owner.prefix") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.developer")) {
-            target2.setDisplayName(getConfig().getString("Developer.prefix") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.manager")) {
-            target2.setDisplayName(getConfig().getString("Manager.prefix") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.admin")) {
-            target2.setDisplayName(getConfig().getString("Admin.prefix") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.seniormod")) {
-            target2.setDisplayName(getConfig().getString("Senior-Mod.prefix") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.mod")) {
-            target2.setDisplayName(getConfig().getString("Mod.prefix") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.trialmod")) {
-            target2.setDisplayName(getConfig().getString("Trial-Mod.prefix") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.builder")) {
-            target2.setDisplayName(getConfig().getString("Builder.prefix") + target2.getName());
-        } else {
-            target2.setDisplayName(getConfig().getString("Default.prefix") + target2.getName());
+        for (String rank : this.ranks) {
+            if (target2.hasPermission(getConfig().getString("Ranks." + rank + ".permission"))) {
+                String prefix = getConfig().getString("Ranks." + rank + ".prefix");
+                target2.setDisplayName(ChatColor.translateAlternateColorCodes('&', prefix + target2.getName()));
+            }
         }
     }
 
     public void setTarget2Color(Player target2) {
-        if (target2.hasPermission("vnitrogen.groups.owner")) {
-            target2.setDisplayName(getConfig().getString("Owner.color") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.developer")) {
-            target2.setDisplayName(getConfig().getString("Developer.color") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.manager")) {
-            target2.setDisplayName(getConfig().getString("Manager.color") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.admin")) {
-            target2.setDisplayName(getConfig().getString("Admin.color") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.seniormod")) {
-            target2.setDisplayName(getConfig().getString("Senior-Mod.color") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.mod")) {
-            target2.setDisplayName(getConfig().getString("Mod.color") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.trialmod")) {
-            target2.setDisplayName(getConfig().getString("Trial-Mod.color") + target2.getName());
-        } else if (target2.hasPermission("vnitrogen.groups.builder")) {
-            target2.setDisplayName(getConfig().getString("Builder.color") + target2.getName());
-        } else {
-            target2.setDisplayName(getConfig().getString("Default.color") + target2.getName());
+        for (String rank : this.ranks) {
+            if (target2.hasPermission(getConfig().getString("Ranks." + rank + ".permission"))) {
+                String color = getConfig().getString("Ranks." + rank + ".color");
+                target2.setDisplayName(ChatColor.translateAlternateColorCodes('&', color + target2.getName()));
+            }
         }
     }
 
@@ -296,6 +222,15 @@ public final class vNitrogen extends JavaPlugin {
         if (data.config.contains("BannedPlayers")) {
             for (String bannedPlayers : data.config.getConfigurationSection("BannedPlayers").getKeys(false)) {
                 banned.add(bannedPlayers);
+            }
+        }
+    }
+
+    private void refreshBlacklists() {
+        if (data.config.contains("BlacklistedIPs")) {
+            for (String uuid : data.config.getConfigurationSection("BlacklistedIPs").getKeys(false)) {
+                String ip = data.config.getString("BlacklistedIPs." + uuid + ".IP");
+                blacklisted.add(ip);
             }
         }
     }
