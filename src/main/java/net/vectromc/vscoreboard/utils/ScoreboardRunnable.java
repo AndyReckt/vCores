1d9463c72cbcbe3188784646767b3bc871cc2a51
Yochran <71285258+Yochran@users.noreply.github.com>
2020-11-15 17:25:49 -0500

    Update ScoreboardRunnable.java

--- src/main/java/net/vectromc/vscoreboard/utils/ScoreboardRunnable.java
+++ src/main/java/net/vectromc/vscoreboard/utils/ScoreboardRunnable.java
@@ -1,6 +1,8 @@
 package net.vectromc.vscoreboard.utils;
 
+import net.vectromc.vnitrogen.vNitrogen;
 import net.vectromc.vscoreboard.VScoreboard;
+import net.vectromc.vstaffutils.vStaffUtils;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
@@ -8,15 +10,21 @@ import org.bukkit.scheduler.BukkitRunnable;
 public class ScoreboardRunnable extends BukkitRunnable {
 
     private VScoreboard plugin;
+    private vStaffUtils staffUtils;
+    private vNitrogen nitrogen;
 
     public ScoreboardRunnable() {
         plugin = VScoreboard.getPlugin(VScoreboard.class);
+        staffUtils = vStaffUtils.getPlugin(vStaffUtils.class);
+        nitrogen = vNitrogen.getPlugin(vNitrogen.class);
     }
 
     @Override
     public void run() {
-        for(Player onlinePlayers : Bukkit.getOnlinePlayers()) {
-            plugin.scoreboard.scoreboard(onlinePlayers);
+        for (Player onlinePlayers : Bukkit.getOnlinePlayers()) {
+            if (!plugin.tsb.contains(onlinePlayers.getUniqueId())) {
+                plugin.scoreboard.scoreboard(onlinePlayers);
+            }
         }
     }
 }
