6e7a399018da3239e3063e6201b84ce14ed79966
Yochran <71285258+Yochran@users.noreply.github.com>
2020-11-17 19:53:32 -0500

    Update BasicCommand.java

--- src/main/java/net/vectromc/vbasic/commands/BasicCommand.java
+++ src/main/java/net/vectromc/vbasic/commands/BasicCommand.java
@@ -17,37 +17,50 @@ public class BasicCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (!(sender instanceof Player)) {
-            Utils.sendMessage(sender, plugin.getConfig().getString("MustBePlayer").replaceAll("%plugin_prefix%", plugin.getConfig().getString("PluginPrefix")).replaceAll("%server_prefix%", plugin.getConfig().getString("ServerPrefix")));
-        } else {
-            Player player = (Player) sender;
-            if (args.length == 0) {
-                Utils.liner(player);
-                Utils.sendMessage(player, "&a&lvBasic");
-                Utils.spacer(player);
-                Utils.sendMessage(player, "&2Plugin By: &b&oYochran");
-                Utils.sendMessage(player, "&2Plugin Version: &a1.0");
-                Utils.liner(player);
-            } else if (args.length == 1) {
-                if (args[0].equalsIgnoreCase("reload")) {
+        if (args.length == 0) {
+            Utils.liner(sender);
+            Utils.sendMessage(sender, "&7[&6&lvBasic&7]:");
+            Utils.spacer(sender);
+            Utils.sendMessage(sender, "&ePlugin By&7: &6&oYochran");
+            Utils.sendMessage(sender, "&ePlugin Version&7: &61.0");
+            Utils.sendMessage(sender, "&6/vbasic reload");
+            Utils.liner(sender);
+        } else if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("reload")) {
+                if (sender.hasPermission(plugin.getConfig().getString("ReloadPermission"))) {
                     plugin.reloadConfig();
-                    Utils.sendMessage(player, plugin.getConfig().getString("ReloadConfig").replaceAll("%server_prefix%", plugin.getConfig().getString("ServerPrefix")).replaceAll("%plugin_prefix%", plugin.getConfig().getString("PluginPrefix")));
+                    Utils.sendMessage(sender, plugin.getConfig().getString("ReloadConfig").replaceAll("%server_prefix%", plugin.getConfig().getString("ServerPrefix")).replaceAll("%plugin_prefix%", plugin.getConfig().getString("PluginPrefix")));
                 } else {
-                    Utils.liner(player);
-                    Utils.sendMessage(player, "&a&lvBasic");
-                    Utils.spacer(player);
-                    Utils.sendMessage(player, "&2Plugin By: &b&oYochran");
-                    Utils.sendMessage(player, "&2Plugin Version: &a1.0");
-                    Utils.liner(player);
+                    Utils.sendMessage(sender, plugin.getConfig().getString("NoPermission").replaceAll("%server_prefix%", plugin.getConfig().getString("ServerPrefix")).replace("%plugin_prefix%", plugin.getConfig().getString("PluginPrefix")));
                 }
+            } else if (args[0].equalsIgnoreCase("help")) {
+                Utils.liner(sender);
+                Utils.sendMessage(sender, "&7[&6&lvBasic&7]: &eHelp:");
+                Utils.sendMessage(sender, "&e ");
+                Utils.sendMessage(sender, "&6&lMisc:");
+                Utils.sendMessage(sender, "&e/gamemode, /feed, /heal, /message, /reply, /settings");
+                Utils.sendMessage(sender, "&e/tpm, /tms, /tgc, /teleport, /tphere, /back");
+                Utils.sendMessage(sender, "&e ");
+                Utils.sendMessage(sender, "&6&lStaff:");
+                Utils.sendMessage(sender, "&e/tsa, /broadcast");
+                Utils.liner(sender);
             } else {
-                Utils.liner(player);
-                Utils.sendMessage(player, "&a&lvBasic");
-                Utils.spacer(player);
-                Utils.sendMessage(player, "&2Plugin By: &b&oYochran");
-                Utils.sendMessage(player, "&2Plugin Version: &a1.0");
-                Utils.liner(player);
+                Utils.liner(sender);
+                Utils.sendMessage(sender, "&7[&6&lvBasic&7]:");
+                Utils.spacer(sender);
+                Utils.sendMessage(sender, "&ePlugin By&7: &6&oYochran");
+                Utils.sendMessage(sender, "&ePlugin Version&7: &61.0");
+                Utils.sendMessage(sender, "&6/vbasic reload");
+                Utils.liner(sender);
             }
+        } else {
+            Utils.liner(sender);
+            Utils.sendMessage(sender, "&7[&6&lvBasic&7]:");
+            Utils.spacer(sender);
+            Utils.sendMessage(sender, "&ePlugin By&7: &6&oYochran");
+            Utils.sendMessage(sender, "&ePlugin Version&7: &61.0");
+            Utils.sendMessage(sender, "&6/vbasic reload");
+            Utils.liner(sender);
         }
         return true;
     }
